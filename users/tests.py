a2b7a7d0ec8569e4c0eb5e85a940d15d78760098
Мария <oms1999@yandex.ru>
2024-07-20 02:58:38 +0300

    Add tests and report

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from users.models import User
+
+
+class UserTest(TestCase):
+    """
+    Тестирование работы с пользователями
+    """
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_user_register(self):
+        """
+        Тест регистрации пользователя
+        """
+
+        url = reverse('users:register')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/user_register_form.html')
+        data = {
+            'phone': '+78008008000',
+            'nick_name': 'Name',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(nick_name='Name').exists())
+
+    def test_user_update(self):
+        """
+        Тест обновления пользователя
+        """
+
+        data = {
+            'phone': '+78008008000',
+            'nick_name': 'Name',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+        self.client.post(reverse('users:register'), data)
+
+        new_user = User.objects.all().filter(nick_name='Name').first()
+        url = reverse('users:profile')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        data = {
+            'phone': '+78008008000',
+            'nick_name': 'Name1',
+        }
+        response = self.client.post(url, data)
+        new_user.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
